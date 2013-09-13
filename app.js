b8da11166f7de61f242590216110d8d0af1efc1e
Sarah Pitts <mccallsmedia@gmail.com>
2013-09-13 15:35:36 -0500

    reconnects routes to index to app

--- app.js
+++ app.js
@@ -9,8 +9,48 @@ app.set('views',__dirname + '/views');
 app.set('view engine', 'ejs'); // so you can render('index')
 
 // render 'index' into 'boilerplate':
-app.get('/',function(req,res,next){
-  res.render('index', { what: 'best', who: 'me' });
+//app.get('/',function(req,res,next){
+//  res.render('index', { what: 'best', who: 'me' });
+//});
+
+ 
+
+app.get('/', function(req, res){
+
+res.render('index.ejs', {title: 'Clever Kitchens'});
+
 });
+
+ 
+
+app.get('/recipes', function(req, res){
+
+res.render('boilerplate.ejs', {
+
+title: 'Clever Kitchens - Recipes',
+
+body: '<h1>All Recipes</h1>'
+
+});
+
+});
+
+ 
+
+app.get('/recipes/:title', function(req, res) {
+
+res.send('<h1>' + req.params.title + '</h1>');
+
+});
+
+ 
+
+app.get('/*', function(req, res) {
+
+res.status(404).render('error.ejs', {title: 'Error'});
+
+});
+
+ 
 
 app.listen(3000);
